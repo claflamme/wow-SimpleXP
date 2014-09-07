2ef20a4b9930c15b1fa4a3bb37aaa49271816533
Cory LaFlamme <me@corylaflamme.com>
2014-09-07 02:12:31 -0400

    Added the beginnings of an addon toolkit with some event binding stuff.

---
+++ bootstrap.lua
@@ -0,0 +1,69 @@
+Bootstrap = {}
+Bootstrap.__index = Bootstrap
+
+function Bootstrap:new(addonName)
+
+  local self = { name = addonName }
+
+  setmetatable(self, Bootstrap);
+
+  -- Create the initial set of frames for a blank addon. Includes a namespaced
+  -- event frame for registering event listeners to.
+  self.Frames = self:createInitialFrames(self.name)
+
+  self.EventHandlers = {}
+
+  return self
+
+end
+
+---
+-- Registers an event handler for a given event name and callback function.
+--
+-- @param eventName
+-- @param callback
+--
+function Bootstrap:on(eventName, callback)
+
+  -- Event handlers are grouped together in arrays by the type of event they
+  -- respond to. If an array for that event type doesn't exist, then create it.
+  if (self.EventHandlers[eventName] == nil) then
+    self.EventHandlers[eventName] = {}
+  end
+
+  tinsert(self.EventHandlers[eventName], callback)
+
+  self.Frames.event:RegisterEvent(eventName)
+  self.Frames.event:SetScript('OnEvent', callback);
+
+end
+
+---
+-- Creates all the frames required for a blank addon and returns a table
+-- containing references to all of them.
+--
+-- @param addonName
+--
+-- @return Table
+--
+function Bootstrap:createInitialFrames()
+
+  local frames = {}
+
+  frames.event = self:createEventFrame()
+
+  return frames
+
+end
+
+---
+-- Creates a new frame for the purpose of assigning event handlers to. All
+-- handlers created for this addon will be bound to the frame.
+--
+-- @param addonName
+--
+-- @return Frame
+--
+function Bootstrap:createEventFrame()
+  return CreateFrame('Frame', self.name .. 'GlobalEventFrame');
+end
